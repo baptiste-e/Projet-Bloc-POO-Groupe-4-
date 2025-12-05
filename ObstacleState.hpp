b7ee1a4a6928ee1ba9c63fe34887ce9201373334
ValentinPleindoux <valentin.pleindoux@viacesi.fr>
2025-12-05 09:28:46 +0100

    Commented ObstacleState.hpp

--- ObstacleState.hpp
+++ ObstacleState.hpp
@@ -3,12 +3,13 @@
 
 #include "CellState.hpp"
 
+// Classe ObstacleState qui définit les cellules obstacles qui ne change jamais d'état durant tout le jeu
 class ObstacleState : public CellState {
 public:
-    // CORRECTION : Mettre 'true' ici pour qu'il apparaisse comme un '1' dans le fichier de sortie
+    // Mettre 'true' ici pour qu'il apparaisse comme un '1' dans le fichier de sortie
     bool isAlive() const override { return true; }
 
-    // CORRECTION : Il renvoie toujours lui-même (immortel)
+    // Il renvoie toujours lui-même (immortel)
     CellState* nextState(int /*aliveNeighbors*/) const override {
         return new ObstacleState(); 
     }
