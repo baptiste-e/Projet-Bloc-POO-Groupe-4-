fe2b24fba16e3dbdc16ab597e67a1451dc1e9db2
ValentinPleindoux <valentin.pleindoux@viacesi.fr>
2025-12-05 08:57:48 +0100

    Commented CellState.hpp

--- CellState.hpp
+++ CellState.hpp
@@ -1,11 +1,12 @@
 #ifndef CELLSTATE_HPP
 #define CELLSTATE_HPP
 
+// Ici on initialise la classe qui va servir à définir l'état des cellules (mort ou vivant)
 class CellState {
 public:
-    virtual ~CellState() = default;
-    virtual bool isAlive() const = 0;
-    virtual CellState* nextState(int aliveNeighbors) const = 0;
+    virtual ~CellState() = default; //Destructeur virtuel
+    virtual bool isAlive() const = 0; // Ici on initialise la méthode isAlive qui vérifie si une cellule est vivante ou morte
+    virtual CellState* nextState(int aliveNeighbors) const = 0; // Ici on initialise la méthode nextState qui va contenir le prochain état des cellules
 };
 
 #endif
