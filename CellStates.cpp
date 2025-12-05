14ac136e8901eb3a1a978e503fd4b8c0a6a9964f
ValentinPleindoux <valentin.pleindoux@viacesi.fr>
2025-12-05 09:06:04 +0100

    Commented CellStates.cpp

--- CellStates.cpp
+++ CellStates.cpp
@@ -2,19 +2,19 @@
 #include "DeadState.hpp"
 
 // Implémentation de AliveState
-CellState* AliveState::nextState(int aliveNeighbors) const {
-    if (aliveNeighbors == 2 || aliveNeighbors == 3) {
-        return new AliveState();
+CellState* AliveState::nextState(int aliveNeighbors) const { // On appelle la méthode nextState depuis aliveState
+    if (aliveNeighbors == 2 || aliveNeighbors == 3) { // On vérifie si le nombre de cellule voisine vivante est de 2 ou 3
+        return new AliveState(); // On retourne la cellule comme vivante
     } else {
-        return new DeadState();
+        return new DeadState(); // On retourne la cellule comme morte
     }
 }
 
 // Implémentation de DeadState
-CellState* DeadState::nextState(int aliveNeighbors) const {
-    if (aliveNeighbors == 3) {
-        return new AliveState();
+CellState* DeadState::nextState(int aliveNeighbors) const { // On appelle encore la méthode nextState depuis deadState
+    if (aliveNeighbors == 3) { // On vérifie si le nombre de cellules voisines vivantes est de 3
+        return new AliveState(); // On retourne la cellule comme vivante
     } else {
-        return new DeadState();
+        return new DeadState(); // On retourne la cellule comme morte
     }
 }
