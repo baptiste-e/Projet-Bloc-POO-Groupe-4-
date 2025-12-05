67540d1261e7b47a54e4ab697419d64cce72aa42
ValentinPleindoux <valentin.pleindoux@viacesi.fr>
2025-12-05 09:33:59 +0100

    Commented SFMLRenderer.hpp

--- SFMLRenderer.hpp
+++ SFMLRenderer.hpp
@@ -4,10 +4,10 @@
 #include <SFML/Graphics.hpp>
 #include "IRenderer.hpp"
 
-class SFMLRenderer : public IRenderer {
+class SFMLRenderer : public IRenderer { // Classe SFMLRenderer qui va servir à avoir un rendu graphique du jeu de la vie
 private:
-    sf::RenderWindow window;
-    int cellSize;
+    sf::RenderWindow window; // Attribut privé pour la page graphique du jeu de la vie
+    int cellSize; // Attribut privé pour la taille des cellules en pixel
 
 public:
     SFMLRenderer(int width, int height, int cellSize = 10);
