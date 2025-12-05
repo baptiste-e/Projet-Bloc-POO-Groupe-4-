60a90eaf735667a7f2dd1f4cc10322a6687ca88f
ValentinPleindoux <valentin.pleindoux@viacesi.fr>
2025-12-05 09:11:34 +0100

    Commented ConwayRules.hpp

--- ConwayRules.hpp
+++ ConwayRules.hpp
@@ -5,9 +5,9 @@
 #include "AliveState.hpp"
 #include "DeadState.hpp"
 
-class ConwayRules : public Rules {
+class ConwayRules : public Rules { // Ici on initialise la classe qui va servir à implémenter les règles de Conway en utilisant les attributs et méthodes publiques de la classe Rules
 public:
-    virtual CellState* computeNextState(const Cell& cell, int aliveNeighbors) const override;
+    virtual CellState* computeNextState(const Cell& cell, int aliveNeighbors) const override; // Ici on appelle la méthode computeNextState qui va appeler le getter d'état des cellules
 };
 
 #endif
