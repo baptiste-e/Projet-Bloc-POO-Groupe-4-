4bd35e68bdf1ab6e20748f381038f1d72b3aa0ed
Baptiste EMMANUELLI <baptiste.emmanuelli@ismans.viacesi.fr>
2025-12-05 09:00:10 +0100

    New comments in AliveState.hpp
    Updated comments for clarity and inheritance explanation.

--- AliveState.hpp
+++ AliveState.hpp
@@ -2,14 +2,15 @@
 #define ALIVESTATE_HPP
 
 #include "CellState.hpp"
-// On RETIRE #include "DeadState.hpp" pour casser la boucle
+// On inclut CellState.hpp pour l'hérédité.
 
-class AliveState : public CellState {
+class AliveState : public CellState { //Hérédité de la classe Cellstate vers AliveState
 public:
     bool isAlive() const override { return true; }
-
+// On a défini une constante = 0 dans CellState que l'on override pour la remplacer
     // On déclare juste la fonction, sans le code (le ; remplace les {})
     CellState* nextState(int aliveNeighbors) const override;
+// Ici on int aliveNeighbors pour définir de futurs voisins potenciellement vivants, de plus on définit le nouvel état des cellules
 };
 
 #endif
